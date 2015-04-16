ff04956a2bfbaea87c0c6edd7f6403db2cb77627
hizonglol <209193@student.pwr.edu.pl>
2015-04-16 09:57:06 +0200

    Nagłówki metod

--- prj/inc/stos.hh
+++ prj/inc/stos.hh
@@ -64,6 +64,13 @@ public:
    *\return - usuwany element.
    */
   int pop();
+
+  /*!
+   *\brief Sortowanie przez scalanie.
+   *\param[in] poczatek - poczatek tablicy.
+   *\param[in] koniec - koniec tablicy.
+   */
+  void Stos::mergesort(int poczatek, int koniec);
 };
 
 #endif
